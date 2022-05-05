01c01ed906ce0cb396440157035830d0dcfeb625
taesonblack4 <82778730+taesonblack4@users.noreply.github.com>
2022-05-05 15:41:17 -0400

    everything as of 5/5/22

--- htmlFile.js
+++ htmlFile.js
@@ -23,5 +23,5 @@ const requestListener = function (req, res) {
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+   console.log(`Server is running on http://${host}:${port}`);
 });
